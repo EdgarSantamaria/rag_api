d101007576fb7edeb52f8b94f006fb3e785c1162
Edgar Santamaria <67978130+EdgarSantamaria@users.noreply.github.com>
2024-09-02 12:43:32 -0400

    fixed bug
    - fixed bug where it was unable to generate an answer for new videos on the first try
    - separated perform_rag into different functions

--- api/main.py
+++ api/main.py
@@ -11,10 +11,8 @@ import tiktoken
 import os
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import PlainTextResponse
-from starlette.middleware.base import BaseHTTPMiddleware
 import time
-from collections import defaultdict
-from typing import Dict
+
 load_dotenv()
 
 app = FastAPI()
@@ -120,92 +118,105 @@ async def perform_rag(youtube_url, query, pinecone_api_key, openai_client):
         )
         if len(vector_exists['matches']) > 0:
             print('Vector exists.')
+            return query_embedding(query, pinecone_index, video_id, namespace, openai_client, index_name)
         else:
             print('Vector does not exist. Creating a new vector store.')
-            tokenizer = tiktoken.get_encoding('p50k_base')
-            # create the length function
-            def tiktoken_len(text):
-                tokens = tokenizer.encode(
-                    text,
-                    disallowed_special=()
-                )
-                return len(tokens)
-            for i in range(3):
-                loader = YoutubeLoader.from_youtube_url(youtube_url)
-                data = loader.load()
-                if data:
-                    print('Data loaded.')
-                    break
-                else:
-                    print('Data not loaded. Retrying...')
-                    time.sleep(1)
-            if not data:
-                raise ValueError('Failed to load data from Youtube.')
-            
-            # Split the text into documents
-            text_splitter = RecursiveCharacterTextSplitter(
-                chunk_size=2000,
-                chunk_overlap=100,
-                length_function=tiktoken_len,
-                separators=["\n\n", "\n", " ", ""]
-            )
-
-            texts = text_splitter.split_documents(data)
-            print(texts)
-            
-            try:
-                vectorstore_from_texts = PineconeVectorStore.from_texts(
-                    [f"Source: {t.metadata['source']} \n\nContent: {t.page_content}" for t in texts],
-                    embeddings,
-                    index_name=index_name,
-                    namespace=namespace,
-                    ids=[f"{video_id}_{i}" for i in range(len(texts))],  # Assign unique IDs to each text chunk
-                    metadatas=[{"Source":t.metadata['source']} for t in texts]
-                )
-                print('Vector store created.')
-            except Exception as e:
-                print('Error creating vector store: ', e)
-                raise HTTPException(status_code=500, detail=str(e))
-
-        print('Creating query embedding...')
-        # Create the query embedding
-        raw_query_embedding = openai_client.embeddings.create(
-            input=query,
-            model="text-embedding-3-small"
-        )
-        query_embedding = raw_query_embedding.data[0].embedding
-
-        print('Finding top matches...')
-        # Query Pinecone for top matches
-        for i in range(3):
-            top_matches = pinecone_index.query(vector=query_embedding, top_k=10, include_metadata=True, namespace=namespace, filter={'Source': {'$eq': video_id}})
-            if len(top_matches['matches']) > 0:
-                break
-            else:
-                print('No matches found. Retrying...')
-                time.sleep(3)
-
-        # Extract contexts from the top matches
-        contexts = [item['metadata']['text'] for item in top_matches['matches']]
-
-        # Augment the query with context
-        augmented_query = "<CONTEXT>\n" + "\n\n-------\n\n".join(contexts[:10]) + "\n-------\n</CONTEXT>\n\n\n\nMY QUESTION:\n" + query
-
-        # Set up the system prompt
-        system_prompt = f"""You are a professional researcher. Use the youtube video to answer any questions."""
-
-        print('Generating response...')
-        # Generate a response using the openrouter_client
-        res = openrouter_client.chat.completions.create(
-            model="google/gemma-2-9b-it:free",
-            messages=[
-                {"role": "system", "content": system_prompt},
-                {"role": "user", "content": augmented_query}
-            ]
-        )
-        print('Returning response: ', res.choices[0].message.content)
-        # Return the content of the response
-        return res.choices[0].message.content
+            store_video_data(youtube_url, index_name, namespace, video_id)
+            return query_embedding(query, pinecone_index, video_id, namespace, openai_client, index_name)
+        
     except Exception as e:
         print('Rag failed: ', e)
         raise HTTPException(status_code=500, detail=str(e))
+
+def store_video_data(youtube_url, index_name, namespace, video_id):
+    tokenizer = tiktoken.get_encoding('p50k_base')
+    # create the length function
+    def tiktoken_len(text):
+        tokens = tokenizer.encode(
+            text,
+            disallowed_special=()
+        )
+        return len(tokens)
+    for i in range(3):
+        loader = YoutubeLoader.from_youtube_url(youtube_url)
+        data = loader.load()
+        if data:
+            print('Data loaded.')
+            break
+        else:
+            print('Data not loaded. Retrying...')
+            time.sleep(1)
+    if not data:
+        raise ValueError('Failed to load data from Youtube.')
+    
+    # Split the text into documents
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=2000,
+        chunk_overlap=100,
+        length_function=tiktoken_len,
+        separators=["\n\n", "\n", " ", ""]
+    )
+
+    texts = text_splitter.split_documents(data)
+    print(texts)
+    
+    try:
+        vectorstore_from_texts = PineconeVectorStore.from_texts(
+            [f"Source: {t.metadata['source']} \n\nContent: {t.page_content}" for t in texts],
+            embeddings,
+            index_name=index_name,
+            namespace=namespace,
+            ids=[f"{video_id}_{i}" for i in range(len(texts))],  # Assign unique IDs to each text chunk
+            metadatas=[{"Source":t.metadata['source']} for t in texts]
+        )
+        print('Vector store created.')
+    except Exception as e:
+        print('Error creating vector store: ', e)
+        raise HTTPException(status_code=500, detail=str(e))
+    
+def query_embedding(query, pinecone_index, video_id, namespace, openai_client , index_name):
+    # Initialize Pinecone
+    pc = Pinecone(api_key=pinecone_api_key)
+    # Connect to Pinecone index
+    pinecone_index = pc.Index(index_name)
+    print('Creating query embedding...')
+    # Create the query embedding
+    raw_query_embedding = openai_client.embeddings.create(
+        input=query,
+        model="text-embedding-3-small"
+    )
+    query_embedding = raw_query_embedding.data[0].embedding
+
+    print('Finding top matches...')
+    # Query Pinecone for top matches
+    for i in range(3):
+        top_matches = pinecone_index.query(vector=query_embedding, top_k=10, include_metadata=True, namespace=namespace, filter={'Source': {'$eq': video_id}})
+        if len(top_matches['matches']) > 0:
+            break
+        else:
+            # Connect to Pinecone index
+            pinecone_index = pc.Index(index_name)
+            print('No matches found. Retrying...')
+            time.sleep(1)
+
+    # Extract contexts from the top matches
+    contexts = [item['metadata']['text'] for item in top_matches['matches']]
+
+    # Augment the query with context
+    augmented_query = "<CONTEXT>\n" + "\n\n-------\n\n".join(contexts[:10]) + "\n-------\n</CONTEXT>\n\n\n\nMY QUESTION:\n" + query
+
+    # Set up the system prompt
+    system_prompt = f"""You are a professional researcher. Use the youtube video to answer any questions."""
+
+    print('Generating response...')
+    # Generate a response using the openrouter_client
+    res = openrouter_client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": augmented_query}
+        ]
+    )
+    print('Returning response: ', res.choices[0].message.content)
+    # Return the content of the response
+    return res.choices[0].message.content
